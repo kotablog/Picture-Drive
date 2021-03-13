5f66578a364a94e568f0285ff50b1ef708abefd6
kotablog <77395128+kotablog@users.noreply.github.com>
2021-03-13 15:12:58 +0900

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Picture Drive</h2>
+    <p><a href="./login.php">ログイン</a></p>
+    <p><a href="./signin.php">新規登録</a></p>
+</body>
+</html>
